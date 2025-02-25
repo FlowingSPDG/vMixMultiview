5595c066de9653eb43cf32ed647fb87503ff04ba
Shugo Kawamura <ruk1ium@gmail.com>
2025-02-25 20:45:08 +0900

    vMix-utility向け改修

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var vars = {};
-var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+const vars = {};
+const parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, (m,key,value) => {
     vars[key] = value;
 });
 
-var vMixSettings = {
-    IP: "127.0.0.1",
+const vMixSettings = {
+    IP: "localhost",
     port: "8088",
     multiViewInput: 0,
     previousProgram: -1,
@@ -12,21 +12,19 @@ var vMixSettings = {
     refreshInterval: 150,
 }
 
-function search(key, keyValue, Array){
-    for (var i=0; i < Array.length; i++) {
-        if (Array[i][key] === keyValue) {
-            return Array[i];
+function search(key, keyValue, arr){
+    for (let i=0; i < arr.length; i++) {
+        if (arr[i][key] === keyValue) {
+            return arr[i];
         }
     }
 }
 
 function toTitleCase(str) {
-    return str.replace(/(?:^|\s)\w/g, function(match) {
-        return match.toUpperCase();
-    });
+    return str.replace(/(?:^|\s)\w/g, match => match.toUpperCase());
 }
 
-var saveSettings = [
+const saveSettings = [
     "zoomY",
     "zoomX",
     "panX",
@@ -36,7 +34,7 @@ var saveSettings = [
     "fontSize",
 ]
 
-var extras = [
+const extras = [
     {
         name: "clock",
         zoomY: 50,
@@ -46,8 +44,8 @@ var extras = [
         color: "#ffffff",
         enabled: false,
         fontSize: 70,
-        template: function(obj){
-            var templateString = `
+        template: (obj) => {
+            const templateString = `
             <div class="multiViewBox" style="
                 height: ${obj.zoomY}%;
                 width:  ${obj.zoomX}%;
@@ -70,8 +68,8 @@ var extras = [
         color: "#ffffff",
         enabled: false,
         fontSize: 70,
-        template: function(obj){
-            var templateString = `
+        template: (obj) => {
+            const templateString = `
             <div class="multiViewBox" style="
                 height: ${obj.zoomY}%;
                 width:  ${obj.zoomX}%;
@@ -93,8 +91,8 @@ var extras = [
         panY: 0,
         enabled: false,
         fontSize: 36,
-        template: function(obj){
-            var templateString = `
+        template: (obj) => {
+            let templateString = `
             <div class="multiViewBox" style="
                 height: ${obj.zoomY}%;
                 width:  ${obj.zoomX}%;
@@ -105,7 +103,7 @@ var extras = [
                     <div class="column">
             `;
             vMixStatuses.forEach((status, i) => {
-                if(i == 3){
+                if(i === 3){
                     templateString += `</div><div class="column">`
                 }
                 if(vMixSettings[status]){
@@ -126,46 +124,44 @@ var extras = [
 ]
 
 
-if(vars["Input"]){
-    vMixSettings.multiViewInput = parseFloat(vars["Input"]) - 1;
- }
+if(vars.Input){
+    vMixSettings.multiViewInput = Number.parseFloat(vars.Input) - 1;
+}
 
- if(vars["Interval"]){
-    vMixSettings.refreshInterval = parseFloat(vars["Interval"]);
- }
- if(vars["Settings"]){
-    var parsedSettings = JSON.parse(decodeURIComponent(vars.Settings))
-    parsedSettings.forEach((extra, i) => {
-        Object.keys(extra).forEach(property =>{
-            extras[i][property] = extra[property]
-        })
-    })
- }
+if(vars.Interval){
+    vMixSettings.refreshInterval = Number.parseFloat(vars.Interval);
+}
+if(vars.Settings){
+    const parsedSettings = JSON.parse(decodeURIComponent(vars.Settings))
+    for (let i = 0; i < parsedSettings.length; i++) {
+        const extra = parsedSettings[i];
+        for (const property in extra) {
+            extras[i][property] = extra[property];
+        }
+    }
+}
 
 setInterval(() => {
-    
-    fetch('http://' + vMixSettings.IP + ':' + vMixSettings.port + '/api')
+    fetch(`http://${vMixSettings.IP}:${vMixSettings.port}/api`)
     .then(response => response.text())
     .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
     .then(data => vMixRefresh(data));
 
 }, vMixSettings.refreshInterval);
 
-var overlayArray = [];
-var multiViewOverlays = [];
-var lastResponse;
-var vMixInputs = []
+let overlayArray = [];
+let multiViewOverlays = [];
+let lastResponse;
+const vMixInputs = []
 
 function log10(x) {
-    if(Math.log(x)*20 == -Infinity){
+    if(Math.log(x)*20 === Number.NEGATIVE_INFINITY){
         return 0;
     }
-    else{
-        return Math.log(x)*21.5;
-    }
+    return Math.log(x)*21.5;
 }
 
-var vMixStatuses = [
+const vMixStatuses = [
     "recording",
     "external",
     "streaming",
@@ -176,19 +172,19 @@ var vMixStatuses = [
 
 function vMixRefresh(data){
     lastResponse = data;
-    var temporaryMultiViewOverlays = []
-    vMixSettings.previewNumber = parseFloat(data.getElementsByTagName("preview")[0].innerHTML)
-    vMixSettings.programNumber = parseFloat(data.getElementsByTagName("active")[0].innerHTML)
+    const temporaryMultiViewOverlays = []
+    vMixSettings.previewNumber = Number.parseFloat(data.getElementsByTagName("preview")[0].innerHTML)
+    vMixSettings.programNumber = Number.parseFloat(data.getElementsByTagName("active")[0].innerHTML)
     vMixSettings.previewKey = data.querySelector(`[number="${vMixSettings.previewNumber}"]`).getAttribute("key")
     vMixSettings.programKey = data.querySelector(`[number="${vMixSettings.programNumber}"]`).getAttribute("key")
 
-    Array.prototype.slice.call(data.getElementsByTagName("input"), 0 ).forEach(function(input, i){
+    Array.prototype.slice.call(data.getElementsByTagName("input"), 0 ).forEach((input, i) => {
         vMixInputs[i] = {}
         vMixInputs[i].title = input.getAttribute("title");
         vMixInputs[i].key = input.getAttribute("key");
         if(input.getAttribute("muted")){
             vMixInputs[i].audio = true
-            vMixInputs[i].muted = input.getAttribute("muted") == "True"
+            vMixInputs[i].muted = input.getAttribute("muted") === "True"
             vMixInputs[i].audioL = log10(input.getAttribute("meterF1") * 100)
             vMixInputs[i].audioR = log10(input.getAttribute("meterF2") * 100)
         }
@@ -207,8 +203,8 @@ function vMixRefresh(data){
     
     overlayArray = Array.prototype.slice.call(data.getElementsByTagName("input")[vMixSettings.multiViewInput].getElementsByTagName("overlay"), 0 );
 
-    if(overlayArray.length == 0){
-        $(".warning").html("No multiview layers on input " + (parseFloat(vMixSettings.multiViewInput) + 1));
+    if(overlayArray.length === 0){
+        $(".warning").html(`No multiview layers on input ${vMixSettings.multiViewInput + 1}`);
         $(".warning").css("display", "flex");
     }
     else{
@@ -217,24 +213,24 @@ function vMixRefresh(data){
 
     
     
-    for(var i = 0; i < overlayArray.length && i < 9; i++){
-        if(parseFloat(overlayArray[i].getAttribute("index")) != 9){
+    for(let i = 0; i < overlayArray.length && i < 9; i++){
+        if(Number.parseFloat(overlayArray[i].getAttribute("index")) !== 9){
 
-            if(overlayArray[i].getElementsByTagName("position")[0] != undefined){
-                var zoomY =     (parseFloat(overlayArray[i].getElementsByTagName("position")[0].getAttribute("zoomY"))   *   100);
-                var zoomX =     (parseFloat(overlayArray[i].getElementsByTagName("position")[0].getAttribute("zoomX"))   *   100);
-                var panY =      (parseFloat(overlayArray[i].getElementsByTagName("position")[0].getAttribute("panY"))    *   100);
-                var panX =      (parseFloat(overlayArray[i].getElementsByTagName("position")[0].getAttribute("panX"))    *   100);
-                var inputKey =  overlayArray[i].getAttribute("key")
-                var inputName =  lastResponse.querySelector(`input[key="${inputKey}"]`).getAttribute("title")
+            if(overlayArray[i].getElementsByTagName("position")[0] !== undefined){
+                const zoomY =     (Number.parseFloat(overlayArray[i].getElementsByTagName("position")[0].getAttribute("zoomY"))   *   100);
+                const zoomX =     (Number.parseFloat(overlayArray[i].getElementsByTagName("position")[0].getAttribute("zoomX"))   *   100);
+                const panY =      (Number.parseFloat(overlayArray[i].getElementsByTagName("position")[0].getAttribute("panY"))    *   100);
+                const panX =      (Number.parseFloat(overlayArray[i].getElementsByTagName("position")[0].getAttribute("panX"))    *   100);
+                const inputKey =  overlayArray[i].getAttribute("key")
+                const inputName =  lastResponse.querySelector(`input[key="${inputKey}"]`).getAttribute("title")
             }
             else{
-                var zoomY =     (100);
-                var zoomX =     (100);
-                var panY =      (0);
-                var panX =      (0);
-                var inputKey =  overlayArray[i].getAttribute("key")
-                var inputName =  lastResponse.querySelector(`input[key="${inputKey}"]`).getAttribute("title")
+                const zoomY =     (100);
+                const zoomX =     (100);
+                const panY =      (0);
+                const panX =      (0);
+                const inputKey =  overlayArray[i].getAttribute("key")
+                const inputName =  lastResponse.querySelector(`input[key="${inputKey}"]`).getAttribute("title")
 
             }
 
@@ -250,14 +246,14 @@ function vMixRefresh(data){
         }
     }
     
-    var forceRefresh = false
-    vMixStatuses.forEach(status => {
-        if(vMixSettings[status] != (data.getElementsByTagName(status)[0].innerHTML == "True")){
-            vMixSettings[status] = data.getElementsByTagName(status)[0].innerHTML == "True"
+    let forceRefresh = false
+    for (const status of vMixStatuses) {
+        if(vMixSettings[status] !== (data.getElementsByTagName(status)[0].innerHTML === "True")){
+            vMixSettings[status] = data.getElementsByTagName(status)[0].innerHTML === "True"
             forceRefresh = true
         }
-    })
-    if(JSON.stringify(temporaryMultiViewOverlays) != JSON.stringify(multiViewOverlays) || forceRefresh){
+    }
+    if(JSON.stringify(temporaryMultiViewOverlays) !== JSON.stringify(multiViewOverlays) || forceRefresh){
         multiViewOverlays = JSON.parse(JSON.stringify(temporaryMultiViewOverlays))
         refresh();
         updateTally(true);
@@ -270,8 +266,8 @@ function vMixRefresh(data){
 function updateVU(){
     overlayArray.forEach((overlay, i) => {
         if(vMixInputs[vMixInputs.findIndex(vMixInputs => vMixInputs.key === overlay.getAttribute("key"))].audio){
-            $($(".outerContainer").children()[i]).find(".actualMeterL").css("clip-path", "inset(" + (Math.floor((100 - vMixInputs[vMixInputs.findIndex(vMixInputs => vMixInputs.key === overlay.getAttribute("key"))].audioL)/3.125)*3.125) + "% 0% 0% 0%)");
-            $($(".outerContainer").children()[i]).find(".actualMeterR").css("clip-path", "inset(" + (Math.floor((100 - vMixInputs[vMixInputs.findIndex(vMixInputs => vMixInputs.key === overlay.getAttribute("key"))].audioR)/3.125)*3.125) + "% 0% 0% 0%)");
+            $($(".outerContainer").children()[i]).find(".actualMeterL").css("clip-path", `inset(${Math.floor((100 - vMixInputs[vMixInputs.findIndex(vMixInputs => vMixInputs.key === overlay.getAttribute("key"))].audioL)/3.125)*3.125}% 0% 0% 0%)`);
+            $($(".outerContainer").children()[i]).find(".actualMeterR").css("clip-path", `inset(${Math.floor((100 - vMixInputs[vMixInputs.findIndex(vMixInputs => vMixInputs.key === overlay.getAttribute("key"))].audioR)/3.125)*3.125}% 0% 0% 0%)`);
             if(vMixInputs[vMixInputs.findIndex(vMixInputs => vMixInputs.key === overlay.getAttribute("key"))].muted){
                 $($(".outerContainer").children()[i]).find(".actualMeterR").css("filter", "grayscale(100%)");
                 $($(".outerContainer").children()[i]).find(".actualMeterL").css("filter", "grayscale(100%)");
@@ -322,28 +318,26 @@ function generateVU(bool, overlay){
     </div>
         `)
     }
-    else{
-        return ""
-    }
+    return ""
 }
 
 function refresh(){
-    var saveArray = []
-    extras.forEach((extra, i) => {
-        if(saveArray[i] == undefined){
+    const saveArray = []
+    for (const extra of extras) {
+        if(saveArray[i] === undefined){
             saveArray[i] = {}
         }
-        saveSettings.forEach(setting =>{
+        for (const setting of saveSettings) {
             saveArray[i][setting] = extra[setting]
-        })
-    });
+        }
+    }
     $("#saveURL").val(`${window.location.href.split('?')[0]}?Settings=${encodeURIComponent(JSON.stringify(saveArray))}`)
     $("#saveURL").focus(function(){
         $(this).select();
     })
     clockTick();
     $(".outerContainer").html("")
-    multiViewOverlays.forEach(overlay => {
+    for (const overlay of multiViewOverlays) {
         $(".outerContainer").append(`
         <div class="multiViewBox" style="
             height: ${overlay.zoomY}%;
@@ -355,26 +349,26 @@ function refresh(){
         ${generateVU(vMixInputs[vMixInputs.findIndex(vMixInputs => vMixInputs.key === overlay.inputKey)].audio, overlay)}
         </div>
         `)
-    });
-    extras.forEach(extra => {
+    }
+    for (const extra of extras) {
     
-        if(extra.enabled == true){
+        if(extra.enabled){
             $(".outerContainer").append(extra.template(extra))
         }
-    });
+    }
     
 }
 
 function updateTally(force){
-    if(vMixSettings.previousPreviewKey != vMixSettings.previewKey || force){
-        if(multiViewOverlays.findIndex(overlay => overlay.inputKey === vMixSettings.previousPreviewKey) != -1){
+        if(vMixSettings.previousPreviewKey !== vMixSettings.previewKey || force){
+        if(multiViewOverlays.findIndex(overlay => overlay.inputKey === vMixSettings.previousPreviewKey) !== -1){
             $($(".outerContainer").children()[multiViewOverlays.findIndex(overlay => overlay.inputKey === vMixSettings.previousPreviewKey)]).removeClass("preview");
         }
         $($(".outerContainer").children()[multiViewOverlays.findIndex(overlay => overlay.inputKey === vMixSettings.previewKey)]).addClass("preview");
         vMixSettings.previousPreviewKey = vMixSettings.previewKey
     }
-    if(vMixSettings.previousProgramKey != vMixSettings.programKey || force){
-        if(multiViewOverlays.findIndex(overlay => overlay.inputKey === vMixSettings.previousProgramKey) != -1){
+    if(vMixSettings.previousProgramKey !== vMixSettings.programKey || force){
+        if(multiViewOverlays.findIndex(overlay => overlay.inputKey === vMixSettings.previousProgramKey) !== -1){
             $($(".outerContainer").children()[multiViewOverlays.findIndex(overlay => overlay.inputKey === vMixSettings.previousProgramKey)]).removeClass("program");
         }
         $($(".outerContainer").children()[multiViewOverlays.findIndex(overlay => overlay.inputKey === vMixSettings.programKey)]).addClass("program");
@@ -382,16 +376,16 @@ function updateTally(force){
     }
 }
 
-let clockTick = () => {
-    let date = new Date();
+const clockTick = () => {
+    const date = new Date();
     let hrs = date.getHours();
     let mins = date.getMinutes();
     let secs = date.getSeconds();
-    hrs = hrs < 10 ? "0" + hrs : hrs;
-    mins = mins < 10 ? "0" + mins : mins;
-    secs = secs < 10 ? "0" + secs : secs;
+    hrs = hrs < 10 ? `0${hrs}` : hrs;
+    mins = mins < 10 ? `0${mins}` : mins;
+    secs = secs < 10 ? `0${secs}` : secs;
 
-    let time = `${hrs}:${mins}:${secs}`;
+    const time = `${hrs}:${mins}:${secs}`;
     if(search("name", "clock", extras).enabled){
         $("#clock").html(time)
     };
@@ -400,19 +394,20 @@ let clockTick = () => {
 clockTick();
 
 function msToTime(s) {
-    var ms = s % 1000;
-    s = (s - ms) / 1000;
-    var secs = s % 60;
-    s = (s - secs) / 60;
-    var mins = s % 60;
-    var hrs = (s - mins) / 60;
+    let time = s;
+    const ms = time % 1000;
+    time = (time - ms) / 1000;
+    const secs = time % 60;
+    time = (time - secs) / 60;
+    const mins = time % 60;
+    const hrs = (time - mins) / 60;
   
-    return hrs.toString().padStart(2,0) + ':' + mins.toString().padStart(2,0) + ':' + secs.toString().padStart(2,0);
+    return `${hrs.toString().padStart(2,0)}:${mins.toString().padStart(2,0)}:${secs.toString().padStart(2,0)}`;
   }
     
 function updateCountdown(){
     if(vMixInputs[vMixSettings.programNumber - 1].video){
-        var timeLeft = parseFloat(vMixInputs[vMixSettings.programNumber - 1].duration) - parseFloat(vMixInputs[vMixSettings.programNumber - 1].position)
+        const timeLeft = Number.parseFloat(vMixInputs[vMixSettings.programNumber - 1].duration) - Number.parseFloat(vMixInputs[vMixSettings.programNumber - 1].position)
         if(timeLeft < 10000 && timeLeft > 1){
             $("#countdown").parent().addClass("program")
             $("#countdown").parent().removeClass("preview")
@@ -439,67 +434,63 @@ setInterval(() => {
     updateCountdown()
 }, 200);
 
-extras.forEach(extra => {
+for (const extra of extras) {
     $(`#${extra.name}ZoomNumber`).val(extra.zoomX/100)
     $(`#${extra.name}ZoomSlider`).val(extra.zoomX/100)
-    $(`#${extra.name}ZoomSlider`).on('input',function(e){
+    $(`#${extra.name}ZoomSlider`).on('input', e => {
         extra.zoomX = e.target.value * 100;
         extra.zoomY = e.target.value * 100;
         $(`#${extra.name}ZoomNumber`).val(e.target.value)
         refresh();
-
     });
-    $(`#${extra.name}ZoomNumber`).on('input',function(e){
+    $(`#${extra.name}ZoomNumber`).on('input', e => {
         extra.zoomX = e.target.value * 100;
         extra.zoomY = e.target.value * 100;
         $(`#${extra.name}ZoomSlider`).val(e.target.value)
         refresh();
-
     });
 
     $(`#${extra.name}PanYNumber`).val(extra.panY/100)
     $(`#${extra.name}PanYSlider`).val(extra.panY/100)
-    $(`#${extra.name}PanYSlider`).on('input',function(e){
+    $(`#${extra.name}PanYSlider`).on('input', e => {
         extra.panY = e.target.value * 100;
         $(`#${extra.name}PanYNumber`).val(e.target.value)
         refresh();
-
     });
-    $(`#${extra.name}PanYNumber`).on('input',function(e){
+    $(`#${extra.name}PanYNumber`).on('input', e => {
         extra.panY = e.target.value * 100;
         $(`#${extra.name}PanYSlider`).val(e.target.value)
         refresh();
-
     });
 
     $(`#${extra.name}PanXNumber`).val(extra.panX/100)
     $(`#${extra.name}PanXSlider`).val(extra.panX/100)
-    $(`#${extra.name}PanXSlider`).on('input',function(e){
+    $(`#${extra.name}PanXSlider`).on('input', e => {
         extra.panX = e.target.value * 100;
         $(`#${extra.name}PanXNumber`).val(e.target.value)
         refresh();
     });
-    $(`#${extra.name}PanXNumber`).on('input',function(e){
+    $(`#${extra.name}PanXNumber`).on('input', e => {
         extra.panX = e.target.value * 100;
         $(`#${extra.name}PanXSlider`).val(e.target.value)
         refresh();
     });
 
     $(`#${extra.name}Enable`).attr("checked", extra.enabled)
-    $(`#${extra.name}Enable`).on('input',function(e){
+    $(`#${extra.name}Enable`).on('input', e => {
         extra.enabled = e.target.checked;
         refresh();
     });
 
     $(`#${extra.name}FontSize`).val(extra.fontSize)
-    $(`#${extra.name}FontSize`).on('input',function(e){
+    $(`#${extra.name}FontSize`).on('input', e => {
         extra.fontSize = e.target.value;
         refresh();
     });
 
     $(`#${extra.name}Color`).val(extra.color)
-    $(`#${extra.name}Color`).on('input',function(e){
+    $(`#${extra.name}Color`).on('input', e => {
         extra.color = e.target.value;
         refresh();
     });
-})
+}
